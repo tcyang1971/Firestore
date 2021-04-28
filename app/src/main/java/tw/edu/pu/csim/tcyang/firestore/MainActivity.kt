36fc778cd9a6cbab16d6e6bec444ff1bba051b3d
tcyang <tcyang@gm.pu.edu.tw>
2021-04-29 07:51:53 +0800

    宣告變數，存放使用者資料，以及MutableMap處理

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
@@ -1,16 +1,20 @@
 package tw.edu.pu.csim.tcyang.firestore
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var db = FirebaseFirestore.getInstance()
+    var user: MutableMap<String, Any> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
