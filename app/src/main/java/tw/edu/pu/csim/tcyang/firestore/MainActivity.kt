79f765997f51ed3e9c8d0dd6d3a0b21fcc7cd6f7
tcyang <tcyang@gm.pu.edu.tw>
2021-04-29 07:22:00 +0800

    建立Firestore連線

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
@@ -1,9 +1,14 @@
 package tw.edu.pu.csim.tcyang.firestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+
+    var db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
