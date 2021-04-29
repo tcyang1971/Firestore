7a183be72cd86d869301ad70d0700c109e0ae93e
tcyang <tcyang@gm.pu.edu.tw>
2021-04-29 08:23:51 +0800

    新增/修改資料

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore/MainActivity.kt
@@ -15,6 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        btnUpdate.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                user["名字"] = edtName.text.toString()
+                user["出生體重"] = edtWeight.text.toString().toInt()
+                db.collection("Users")
+                        //.document(edtName.text.toString())
+                        //.set(user)
+                        .add(user)
+                        .addOnSuccessListener {
+                            Toast.makeText(baseContext, "新增/異動資料成功",
+                                    Toast.LENGTH_LONG).show()
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(baseContext, "新增/異動資料失敗：" + e.toString(),
+                                    Toast.LENGTH_LONG).show()
+                        }
+            }
+        })
     }
 }
